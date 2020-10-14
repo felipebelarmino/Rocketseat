7a86502701ff2a0b5680134eedc442e5404c406c
Felipe Gomes Belarmino <epilefgomes1@gmail.com>
2020-10-14 16:00:13 -0300

    add image galery selector

--- public/scripts/page-orphanage.js
+++ public/scripts/page-orphanage.js
@@ -29,3 +29,26 @@ L.
 marker([-23.5434615,-46.3148178],{icon}).
 addTo(map)
 
+// image gallery
+function selectImage(event){
+    const button = event.currentTarget
+
+      
+    //remover todas as classes .active
+    const buttons = document.querySelectorAll(".images button")
+    buttons.forEach(removeActiveClass)
+
+    function removeActiveClass(button){
+        button.classList.remove("active")
+    }
+
+    //selecionar a imagem clicada
+    const image=button.children[0]
+    const imageContainer=document.querySelector(".orphanage-details > img")
+
+    //atualizar o container de imagem
+    imageContainer.src=image.src
+
+    //adicionar a classe .active para este botão que foi clicado
+    button.classList.add("active")
+}
